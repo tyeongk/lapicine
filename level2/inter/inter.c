c5f1119e1054421594afc035869c3d6e111209e1
Taeyeong Kang <taeykang@c2r10s6.42gyeongsan.kr>
2025-02-13 16:49:47 +0900

    5

--- level2/inter/inter.c
+++ level2/inter/inter.c
@@ -6,7 +6,7 @@
 /*   By: taeykang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 14:25:30 by taeykang          #+#    #+#             */
-/*   Updated: 2025/02/13 15:03:31 by taeykang         ###   ########.fr       */
+/*   Updated: 2025/02/13 16:16:43 by taeykang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,29 @@ void	ft_putchar(char c)
 
 int	inter(char *str1, char *str2)
 {
+	int	ascii[256] = {0};
 	int	i;
-	int	same;
-
+	
 	i = 0;
-	while(*str1 != '\0' || *str2 != '\0')
+	while(str2[i])
 	{
-		if(str1[i] == str2[i])
-			same = str1[i];
-		ft_putchar(same);
+		if(ascii[(int)str2[i]] == 0)
+			ascii[(int)str2[i]] = 1;
 		i++;
-		else if(str1[i] != str2[i])
-			i++;
 	}
-	return (0);	
+	while(str1[i])
+	{
+		if(ascii[(int)str1[i]] == 1)
+			ascii[(int)str1[i]] = 2;
+		write(1, str1[i], 1);
+		i++;
+	}
 }	
 
 int	main(int ac, char **av)
 {
-	if(av != 3)
-		return (0);
-
-	if(ac == 2)
-	{
-		inter(,);
-	}
-	ft_putchar('\n');
+	if(ac == 3)
+		inter(av[1], av[2]);
+	write(1, '\n', 1);
+	return (0);
 }
